2ddaf1bc009e46487504a7966a494d9bf01246bd
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-06-09 10:13:21 +0900

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// http://bit.ly/vuelint-rules
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: "module"
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: "standard",
+  // required to lint *.vue files
+  plugins: ["html"]
+};
